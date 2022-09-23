3cc1433f5c29176ba1dd3729a72c91c2aa9cb834
Matheus Ferreira <matheus.aferreira@mercadolivre.com>
2022-09-23 16:37:20 -0300

    feat: exercise 3 and 4 of integrative practice 01 - instantiated 3 objects with diff parameters

--- src/praticaIntegradora1/App.java
+++ src/praticaIntegradora1/App.java
@@ -1,4 +1,9 @@
 package praticaIntegradora1;
 
 public class App {
+    public static void main(String[] args) {
+        Person personWithoutAttrs = new Person();
+        Person personWithNameAndAge = new Person("Matheus", 20, "id-01");
+        Person personWithAllAttrs = new Person("Matheus", 20, "id-01", 60, 1.70);
+    }
 }
--- src/praticaIntegradora1/Person.java
+++ src/praticaIntegradora1/Person.java
@@ -4,12 +4,12 @@ public class Person {
     String name;
     int age;
     String id;
-    float heavy;
-    float height;
+    double heavy;
+    double height;
 
     public Person() {}
 
-    public Person(String name, int age, int id) {}
+    public Person(String name, int age, String id) {}
 
-    public Person(String name, int age, int id, float heavy, float height) {}
+    public Person(String name, int age, String id, double heavy, double height) {}
 }
